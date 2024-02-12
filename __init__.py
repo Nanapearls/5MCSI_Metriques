a79648c1d263c7659819d9b38c2200c64c15acb5
Nanapearls <85940561+Nanapearls@users.noreply.github.com>
2024-02-12 18:11:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def hello_world():
     return render_template('hello.html')#commentaires
 @app.route("/contact/")
 def contact():
-    return render_template('.html') 
+    return render_template('nana.html') 
   
 if __name__ == "__main__":
   app.run(debug=True)
